f2bba5ae2c8f9fd479ad05198951dabc3faf77ef
Rob Cowie <rob_cowie@mac.com>
2012-11-30 17:39:17 +0000

    Fail on python2.6

--- awesome/sauce.py
+++ awesome/sauce.py
@@ -1,9 +1,13 @@
 # -*- coding: utf8 -*-
 
 import random
+import sys
 
 
 class Cherry(object):
 
     def random(self):
         return random.randint(1, 1000)
+        ## Fail on py26
+        if sys.version_info.minor == 6:
+            raise Exception()
